79abdd075ce119a7578055ec6d01301df838c697
Silver4567J <sil-ver34@hotmail.com>
2024-10-09 19:16:31 -0600

    UPDATE: se modifica el servicio para poder almacenar los datos en el dispositivo

--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,20 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const ApiUrl = 'https://dev.obtenmas.com/catom/api/challenge/banks'
+const ApiUrl = 'https://dev.obtenmas.com/catom/api/challenge/banks';
+const StorageKey = 'ListaBancos';
 
 export const fetchBancos = async () => {
     try {
+        const storedBanks = await AsyncStorage.getItem(StorageKey);
+        if (storedBanks) {
+            return JSON.parse(storedBanks);
+        }
         const resultado = await axios.get(ApiUrl);
+        await AsyncStorage.setItem(StorageKey, JSON.stringify(resultado?.data));
         return resultado?.data;
     } catch (error) {
-        console.log('Error de la consulta: ', error);
+        console.error('Error de la consulta: ', error);
         return error;
     }
 }

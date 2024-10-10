467dff80e35783fcd33c74df60342986baf71e79
Silver4567J <sil-ver34@hotmail.com>
2024-10-09 23:06:05 -0600

    FEATURE: se añde el componente boton

---
+++ src/components/Button/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    button: {
+        backgroundColor: '#007bff',
+        borderRadius: 20,
+        marginVertical: 20,
+        width: '90%',
+        alignSelf: 'center',
+        alignItems: 'center',
+        height: 50,
+        justifyContent: 'center'
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+        textAlign: 'center',
+    },
+});

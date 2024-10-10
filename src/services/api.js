6971aca14f281beaf783f738574ce6a217c22018
Silver4567J <sil-ver34@hotmail.com>
2024-10-09 23:08:55 -0600

    UPDATE: se modifica para la fincionalida de la lista de los bancos

--- src/services/api.js
+++ src/services/api.js
@@ -6,15 +6,34 @@ const StorageKey = 'ListaBancos';
 
 export const fetchBancos = async () => {
     try {
-        const storedBanks = await AsyncStorage.getItem(StorageKey);
-        if (storedBanks) {
-            return JSON.parse(storedBanks);
-        }
         const resultado = await axios.get(ApiUrl);
         await AsyncStorage.setItem(StorageKey, JSON.stringify(resultado?.data));
         return resultado?.data;
     } catch (error) {
         console.error('Error de la consulta: ', error);
-        return error;
+        throw error;
     }
 }
+
+export const fetchBancosAlmacenado = async () => {
+    try {
+        const bancoAlmacenado = await AsyncStorage.getItem(StorageKey);
+        if (bancoAlmacenado) {
+            return JSON.parse(bancoAlmacenado);
+        } else {
+            return await fetchBancos();
+        }
+    } catch (error) {
+        console.error('Fallo en el alamcenamiento de los bancos: ', error);
+        throw error
+    }
+};
+
+export const limpiarAlmacenamiento = async () => {
+    try {
+        await AsyncStorage.removeItem(StorageKey);
+    } catch (error) {
+        console.error('Fallo al borrar la informacion: ', error);
+        throw error;
+    }
+};

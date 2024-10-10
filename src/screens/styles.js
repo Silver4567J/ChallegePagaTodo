a9bef6e6fd0a632113c064fa30bf5cc6078f2ae8
Silver4567J <sil-ver34@hotmail.com>
2024-10-09 23:09:49 -0600

    STYLE: se crea la hoja de estilos para la pantalla principal

---
+++ src/screens/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'red'
+    },
+    boxText: {
+        paddingHorizontal: 20
+    },
+    textNoInfo: {
+        textAlign: 'center',
+        fontSize: 20,
+    }
+})
